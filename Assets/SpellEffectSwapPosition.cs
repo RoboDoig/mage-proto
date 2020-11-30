9a6070e0fdcf439f801829ea5a19f4c284a54c17
Andrew Erskine <erskine94@gmail.com>
2020-11-29 19:56:40 -0800

    Delete redundant code

--- Assets/SpellEffectSwapPosition.cs
+++ Assets/SpellEffectSwapPosition.cs
@@ -3,34 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpellEffectSwapPosition : SpellEffect
-{
-    public override bool OnInitiate(CharacterControl caster, Vector3 initiateTarget, Vector3 castTarget, Transform wandCastTransform) {
-        currentInitiateIndicator = Instantiate(initiateIndicator, wandCastTransform.position, Quaternion.identity);
-        currentInitiateIndicator.transform.SetParent(wandCastTransform);
-        return true;
-    }
-
-    public override bool OnRelease(CharacterControl caster, Vector3 initiateTarget, Vector3 castTarget, Transform wandCastTransform) {
-        Destroy(currentInitiateIndicator);
-        OnEffectStart(caster, initiateTarget, castTarget, wandCastTransform);
-        return true;
-    }
-    
-    public override bool OnEffectStart(CharacterControl caster, Vector3 initiateTarget, Vector3 castTarget, Transform wandCastTransform) {
-        currentProjectile = Instantiate(projectile, wandCastTransform.position, Quaternion.identity);
-        return true;
-    }
-
-    public override bool EffectUpdate(CharacterControl caster, Vector3 initiateTarget, Vector3 castTarget) {
-        currentProjectile.transform.position = Vector3.MoveTowards(currentProjectile.transform.position, castTarget, 0.1f);
-        if ((currentProjectile.transform.position - castTarget).magnitude < 0.1f) {
-            OnEffectEnd(caster, initiateTarget, castTarget);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
+{   
     public override bool OnEffectEnd(CharacterControl caster, Vector3 initiateTarget, Vector3 castTarget) {
         Destroy(currentProjectile);
         currentImpactIndicator = Instantiate(impactIndicator, castTarget, Quaternion.identity);
